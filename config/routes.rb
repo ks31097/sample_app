729d9160bab791850ca2473c4c02732e92fbca89
Kostya Sigarov <ksigaryov@gmail.com>
2022-07-13 15:48:54 +0300

    Update path in the routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
-  get 'help' => 'static_pages#help'
-  get 'about' => 'static_pages#about'
+  get 'help', to: 'static_pages#help' # <=> get 'help' => 'static_pages#help'
+  get 'about', to: 'static_pages#about' # get 'about' => 'static_pages#about'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
