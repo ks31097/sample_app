d12207892c0a85a98d8481cd74379fbed8f1f9cb
Kostya Sigarov <ksigaryov@gmail.com>
2022-07-13 16:02:44 +0300

    Added "Contact" page

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root 'static_pages#home'
   get 'help', to: 'static_pages#help' # <=> get 'help' => 'static_pages#help'
   get 'about', to: 'static_pages#about' # get 'about' => 'static_pages#about'
+  get 'contact', to: 'static_pages#contact' # get 'contact' => 'static_pages#contact'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
