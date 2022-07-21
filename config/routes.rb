0bea37c952e864a7331f39aa3817277f0710e1d8
Kostya Sigarov <ksigaryov@gmail.com>
2022-07-21 22:13:50 +0300

    Added user resources in the routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   get 'about', to: 'static_pages#about' # get 'about' => 'static_pages#about'
   get 'contact', to: 'static_pages#contact' # get 'contact' => 'static_pages#contact'
   get 'signup', to: 'users#new'
+  resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
