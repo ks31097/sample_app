3937e09d29732f3bab1f5b10ca8b2030816c552c
Kostya Sigarov <ksigaryov@gmail.com>
2022-07-22 11:09:46 +0300

    Update login path in the routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,10 @@ Rails.application.routes.draw do
   get 'about', to: 'static_pages#about' # get 'about' => 'static_pages#about'
   get 'contact', to: 'static_pages#contact' # get 'contact' => 'static_pages#contact'
   get 'signup', to: 'users#new'
-  resources :users
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+  resources :user
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
